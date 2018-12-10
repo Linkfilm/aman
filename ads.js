edce22fc93921b81d566e7176796091d85c9a5fd
Safe Movie <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-12-10 16:53:48 +0700

    Update and rename phanton.js to ads.js

---
+++ ads.js
@@ -0,0 +1,10 @@
+<script src="/ads.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+if(document.getElementById('WDdKxLHgeIUr')){
+  alert('Blocking Ads: No');
+} else {
+  alert('Blocking Ads: Yes');
+}
+
+</script>
